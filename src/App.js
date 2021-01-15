32e72d8e9a5651add11f640c7fb13bb336defba2
Janara Tolonbaeva <janaratolonbaeva@gmail.com>
2021-01-15 12:09:40 +0600

    add alert.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, {useState} from 'react';
 import './App.css';
 import Modal from "./components/UI/Modal/Modal";
 import Button from "./components/UI/Button/Button";
+import Alert from "./components/UI/Alert/Alert";
 
 const App = () => {
   const [show, setShow] = useState(false);
-  const [alertShow, setAlertShow] = useState(false);
 
   const modalShow = () => {
     setShow(true);
@@ -24,20 +24,34 @@ const App = () => {
     {type: 'btn-danger', label: 'Close', clicked: modalClosed}
   ];
 
+  const clickDismissible = () => {
+    console.log('dismiss');
+  }
+
   return (
     <>
+      <Modal
+        show={show}
+        closed={modalClosed}
+        title="Modal title"
+        text="Modal body text goes here."
+        btnArr={btnArr}
+      />
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-4 mt-5 text-center">
               <Button btnClass="btn btn-primary" clicked={modalShow}>Open</Button>
           </div>
-        <Modal
-          show={show}
-          closed={modalClosed}
-          title="Modal title"
-          text="Modal body text goes here."
-          btnArr={btnArr}
-        />
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-8 mt-5">
+            <Alert type="warning" dismiss={clickDismissible}>This is a warning type alert</Alert>
+            <Alert type="success">This is a success type alert</Alert>
+            <Alert type="secondary">This is a secondary type alert</Alert>
+            <Alert type="danger">This is a danger type alert</Alert>
+            <Alert type="info">This is a info type alert</Alert>
+            <Alert type="light" dismiss={clickDismissible}>This is a light type alert</Alert>
+          </div>
         </div>
       </div>
     </>
--- src/components/UI/Alert/Alert.js
+++ src/components/UI/Alert/Alert.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Alert = ({type, children}) => {
-	return (
-			<div
-				className={['alert', type].join(' ')}
-				role="alert"
-				style={{
+const Alert = ({type, children, dismiss}) => {
+	const [isOpen, setOpen] = useState(true);
 
-				}}
-			>
+	const classes = ['alert', 'alert-' + type];
+
+	if (dismiss) {
+		classes.push('alert-dismissible');
+	}
+
+	const closed = () => {
+		setOpen(false);
+		dismiss();
+	}
+
+	return isOpen && (
+		<div className={classes.join(' ')}>
 			{children}
+			{dismiss && <button className="btn-close" onClick={closed}/>}
 		</div>
-	);
-};
+	)
+}
 
 export default Alert;
